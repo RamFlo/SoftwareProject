4a6984cf8041c7aa8440d2cbf7e5e036106be769
Ram S <ramsh92@gmail.com>
2018-03-03 17:42:41 +0200

    a

--- Chess/Chess/ChessGame.c
+++ Chess/Chess/ChessGame.c
@@ -389,24 +389,42 @@ char spFiarCheckWinner(SPFiarGame* src) {
 
 
 bool blockedPathCheck(ChessGame* src, int r1_n, int c1_n, int r2_n, int c2_n) {
-	int start = 0, end = 0, i = 0;
+	int i = 0, j = 0, lower_r = 0, left_c = 0, upper_r = 0, right_c = 0, diag_type = 0, uppercase = 0;
+	lower_r = (r1_n < r2_n) ? r1_n : r2_n;
+	upper_r = (r1_n < r2_n) ? r2_n : r1_n;
+	left_c = (c1_n < c2_n) ? c1_n : c2_n;
+	right_c = (c1_n < c2_n) ? c2_n : c1_n;
+	diag_type = ((r1_n < r2_n &&c1_n < c2_n) || (r1_n > r2_n &&c1_n > c2_n)) ? 1 : -1;
 	if (c1_n == c2_n) {		//forward
-		start = (r1_n < r2_n) ? (r1_n + 1) : (r2_n + 1);
-		end = (r1_n < r2_n) ? (r2_n - 1) : (r1_n - 1);
-		for (i = start; i <= end; i++) {
+		for (i = lower_r + 1; i <= upper_r - 1; i++) {
 			if (src->gameBoard[i][c1_n] != '\0')
 				return true;
 		}
 	}
 	else if (r1_n == r2_n) {		//sideways
-		start = (c1_n < c2_n) ? (c1_n + 1) : (c2_n + 1);
-		end = (c1_n < c2_n) ? (c2_n - 1) : (c1_n - 1);
-		for (i = start; i <= end; i++) {
+		for (i = left_c + 1; i <= right_c + 1; i++) {
 			if (src->gameBoard[r1_n][i] != '\0')
 				return true;
 		}
 	}
-	else {
-
+	else if (diag_type == 1) {		//diag: '/'
+		for (i = 1; i <= upper_r - lower_r - 1; i++) {
+			if (src->gameBoard[lower_r + i][left_c + i] != '\0')
+				return true;
+		}
 	}
+	else if (diag_type == -1) {		//diag: '\'
+		for (i = 1; i <= upper_r - lower_r - 1; i++) {
+			if (src->gameBoard[lower_r + i][right_c - i] != '\0')
+				return true;
+		}
+	}
+	if (src->gameBoard[r2_n][c2_n] != '\0') { //check current player obstacle at destination
+		uppercase = isupper(src->gameBoard[r2_n][c2_n]);
+		if (src->currentPlayer == WHITE_PLAYER)
+			return (!uppercase);
+		else
+			return uppercase;
+	}
+	return false;
 }
